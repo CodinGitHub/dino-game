b1dc9c9b2e937bab8e9feccc2ea4a60c26605ddf
CodinGitHub <gms2programador@gmail.com>
2022-09-15 08:16:34 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// GAME LOOP
+
+let time = new Date();
+let deltaTime = 0;
+
+if(document.readyState === 'complete' || document.readyState === 'interactive'){
+    setTimeout(Init, 1);
+}else{
+    document.addEventListener('DOMContentLoaded', Init);
+}
+
+function Init(){
+    time = new Date();
+    Start();
+    Loop();
+}
+
+function Loop(){
+    deltaTime = (new Date() - time) / 1000;
+    time = new Date();
+    
+}
+
+function update(){
+
+}
